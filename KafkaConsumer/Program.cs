94652aef5a632ce89bf73a268d88e42e2276ced8
guilhermepereira <guilhermepereira@uniaraxa.edu.br>
2020-11-26 21:20:25 -0300

    first commit

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,51 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+namespace KafkaConsumer
+{
+    class Program
+    {
+        static void Main()
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "groupo-consumer-teste",
+                BootstrapServers = "127.0.0.1:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using var c = new ConsumerBuilder<Ignore, string>(conf).Build();
+
+            {
+                c.Subscribe("topico-teste");
+
+                var cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = c.Consume(cts.Token);
+                            Console.WriteLine($"Mensagem Recebida '{cr.Message.Value}' em: '{cr.TopicPartitionOffset}'.");
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Ocorreu o erro: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    c.Close();
+                }
+            }
+        }
+    }
+}
